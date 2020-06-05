440c31c97c16a7a09fcd72f286211aa7466ca97c
ZiheHuang <676814617@qq.com>
2020-06-05 21:18:47 +0800

    2

---
+++ src/main/java/com/example/demo/hello.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import java.sql.SQLOutput;
+
+public class hello {
+    int a =0;
+    int b =1;
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
